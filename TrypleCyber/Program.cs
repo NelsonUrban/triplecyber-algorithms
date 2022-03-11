8a83e8fd4764040ec6423fb8b4bdaeefb5d517c2
Nelson Nehemias Urban Marten <nurban@mardom.com>
2022-03-11 07:33:17 -0400

    adding consoles writeline to ask the users parameter in the console

--- TrypleCyber/Program.cs
+++ TrypleCyber/Program.cs
@@ -6,23 +6,43 @@ namespace TrypleCyber
     {
         static void Main(string[] args)
         {
-            //1.	Valores Aleatorios
-            RandomValues random = new RandomValues();
-            var result = random.GetRandomList(10);
 
-            //2.	números primos
-            PrimeNumbers primeNumbers = new PrimeNumbers();
-            var resultPrime = primeNumbers.GeneratePrimeNumbers();
+            Console.WriteLine("Random Values | Enter the number of Random values you want to generate.");
+            var randon = Convert.ToInt32(Console.ReadLine());
+            RandomValues random = new();
+            var result = random.GetRandomList(randon);
+            Console.WriteLine("This is your list of random numbers: ");
+            result.ForEach(delegate (int number)
+            {
+                Console.WriteLine(number);
+            });
 
-            //3.	Emulador de Cajero automático
-            CashMachine cashMachine = new CashMachine();
-            cashMachine.GenerateCash(2537);
 
-            //4.	Coincidencias
-            Coincidences coincidences = new Coincidences();
+            Console.WriteLine("prime numbers | Enter the number of Random values you want to generate.");
+            var prime = Convert.ToInt32(Console.ReadLine());
+            PrimeNumbers primeNumbers = new();
+            var resultPrime = primeNumbers.GeneratePrimeNumbers(prime);
+            Console.WriteLine("This is your list of Prime numbers: ");
+            resultPrime.ForEach(delegate (int number)
+            {
+                Console.WriteLine(number);
+            });
+
+
+            Console.WriteLine("ATM Emulator | Enter the amount of money to withdraw.");
+            var cash = Convert.ToInt32(Console.ReadLine());
+            CashMachine cashMachine = new();
+            var resultCash = cashMachine.GenerateCash(cash);
+            Console.WriteLine("This is their distribution of Cash: ");
+            resultCash.ForEach(delegate (int number)
+            {
+                Console.WriteLine(number);
+            });
+
+            Console.WriteLine("Coincidences | Now we will generate the matches between random numbers and prime numbers and the Fibonacci series");
+            Coincidences coincidences = new();
             coincidences.GenerateCondicences(resultPrime, result);
-
-            Console.WriteLine();
+           
             Console.ReadLine();
         }
     }
