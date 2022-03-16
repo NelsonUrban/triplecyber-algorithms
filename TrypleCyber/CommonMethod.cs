160e6ae65d4f355d42a0687413f7c768990e6063
Nelson Nehemias Urban Marten <nurban@mardom.com>
2022-03-16 10:10:09 -0400

    adding common class

---
+++ TrypleCyber/CommonMethod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrypleCyber
+{
+    public class CommonMethod
+    {
+        public CommonMethod()
+        {
+
+        }
+        public void ShowResult (List<int> list)
+        {
+            list.ForEach(delegate (int number)
+            {
+                Console.WriteLine("This is your Result :"  + number);
+            });
+
+        }
+    }
+}
